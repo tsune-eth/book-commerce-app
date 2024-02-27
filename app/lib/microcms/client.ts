6701ea8e6a8b88536612a6db5bf914e2f81d0369
tsune-eth <tsunekun@yicr.info>
2024-02-27 16:20:46 +0900

    SSR

--- app/lib/microcms/client.ts
+++ app/lib/microcms/client.ts
@@ -9,6 +9,9 @@ export const client = createClient({
 export const getAllBooks = async () => {
     const allBooks = await client.getList<BookType>({
         endpoint: 'ebooks2',
+        customRequestInit: {
+          cache: "no-store"
+        },
     });
     return allBooks;
 }
@@ -17,6 +20,9 @@ export const getDetailBook = async (contentId: string) => {
   const detailBook = await client.getListDetail<BookType>({
     endpoint: 'ebooks2',
     contentId,
+    customRequestInit: {
+      cache: "no-store"
+    },
   });
 
   return detailBook;
